eabf2eff3f31feda9bfadd837e14ecb202739c52
marcpetitsals <marcpetitsalas@gmail.com>
2026-02-12 20:39:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,24 @@ function toggleRadio() {
     button.textContent = "▶ Escuchar";
   }
 }
+// URL de tu Worker
+const NEWS_URL = https://misty-bread-a143.marcpetitsalas.workers.dev;
+
+fetch(NEWS_URL)
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("news");
+    container.innerHTML = "";
+
+    data.forEach(title => {
+      const div = document.createElement("div");
+      div.className = "news-item";
+      div.textContent = title;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => {
+    console.error("Error cargando noticias:", err);
+    document.getElementById("news").textContent = "No se pudieron cargar las noticias.";
+  });
+
