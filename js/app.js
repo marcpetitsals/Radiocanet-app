f89638268d97aa9c2ffd8c4caa5efcbf09f66c8a
marcpetitsals <marcpetitsalas@gmail.com>
2026-02-12 20:13:05 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("App Radio Canet cargada");
+function toggleRadio() {
+  const player = document.getElementById("radioPlayer");
+  const button = document.getElementById("playButton");
+
+  if (player.paused) {
+    player.play();
+    button.textContent = "⏸ Pausar";
+  } else {
+    player.pause();
+    button.textContent = "▶ Escuchar";
+  }
+}
